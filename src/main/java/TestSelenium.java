e5b93d22dfeb4c14ac57532155f2704874a6e28b
simiaoNetshoes <Sonnyk23>
2018-08-16 13:40:04 -0300

    Initial commit

---
+++ src/main/java/TestSelenium.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestSelenium {
+
+    @Test
+    public void initTest() {
+        Assert.assertTrue(true);
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+        Assert.assertFalse(false);
+    }
+    @Test
+    public void mediumTest() {
+        Assert.assertTrue(true);
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+        Assert.assertFalse(false);
+    }
+    @Test
+    public void secondTest() {
+        Assert.assertTrue(true);
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+        Assert.assertFalse(false);
+    }
+    @Test
+    public void finalTest() {
+        Assert.assertTrue(true);
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+        Assert.assertFalse(false);
+    }
+
+}
